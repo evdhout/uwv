cc2d3fc0397486ed01f98695440029fbac55391f
Erik van den Hout <5578847+evdhout@users.noreply.github.com>
2024-05-25 19:15:55 +0200

    Fix logical error
    If remote has not changed, get_cbs_opendata_table would always get the data.

--- uwv/data/cbs/get_cbs_opendata_table.py
+++ uwv/data/cbs/get_cbs_opendata_table.py
@@ -25,8 +25,11 @@ def get_cbs_opendata_table(
         logger.debug(f"Creating CBS OpenData table {cbs_table_id} data path: {cbs_table_path}")
         cbs_table_path.mkdir(exist_ok=True, parents=True)
     else:
-        if is_cbs_opendata_table_updated(cbs_table_id) and not overwrite:
-            logger.error(f"New version online but {overwrite=}, tables not updated")
+        if is_cbs_opendata_table_updated(cbs_table_id):
+            if not overwrite:
+                logger.error(f"New version online but {overwrite=}, tables not updated")
+                return False
+        else:
             return False
 
     download_cbs_opendata_table(cbs_table_id)
