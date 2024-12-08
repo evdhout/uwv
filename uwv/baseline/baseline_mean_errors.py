2a0bc664b675305552148b085f3fbafef18bc146
Erik van den Hout <5578847+evdhout@users.noreply.github.com>
2024-12-08 13:44:49 +0100

    Refactor logging statements for consistency
    Streamlined logging statements to a single line for each file format. This change enhances code readability and maintains consistency in logging practices.

--- uwv/baseline/baseline_mean_errors.py
+++ uwv/baseline/baseline_mean_errors.py
@@ -78,13 +78,9 @@ def main(force: bool = False):
     merged_df = merged_df.reset_index()
 
     merged_df.to_parquet(BASELINE_MEAN_ERRORS_PARQUET, index=False)
-    logger.info(
-        f"Saved {BASELINE_MEAN_ERRORS_PARQUET} with index=False to avoid duplicate index"
-    )
+    logger.info(f"Saved {BASELINE_MEAN_ERRORS_PARQUET} with index=False to avoid duplicate index")
     merged_df.to_csv(BASELINE_MEAN_ERRORS_CSV, index=False)
-    logger.info(
-        f"Saved {BASELINE_MEAN_ERRORS_CSV} with index=False to avoid duplicate index"
-    )
+    logger.info(f"Saved {BASELINE_MEAN_ERRORS_CSV} with index=False to avoid duplicate index")
 
 
 if __name__ == "__main__":
